771606e8cf541e57fe39897e8d6ad8492e598333
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-11 04:49:37 +0700

    add Link for navigation, add some logic, add categories and add new states.

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,20 +1,54 @@
 import { Autocomplete, TextField } from '@mui/material'
+import { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import MyButton from './commons/MyButton'
 import MyContainer from './commons/MyContainer'
 import Wrapper from './commons/Wrapper'
 
 const CategorySection = () => {
+	const categories = [
+		'animal',
+		'career',
+		'celebrity',
+		'dev',
+		'explicit',
+		'fashion',
+		'food',
+		'history',
+		'money',
+		'movie',
+		'music',
+		'political',
+		'religion',
+		'science',
+		'sport',
+		'travel',
+	]
+	const [category, setCategory] = useState(() => null)
+	const [inputCategory, setInputCategory] = useState(() => '')
+	const uhm = useRef()
 	return (
 		<MyContainer position='absolute'>
 			<Wrapper justifyContent='space-between'>
 				<Autocomplete
+					ref={uhm}
+					value={category}
+					onChange={(event, newValue) => {
+						setCategory(() => newValue)
+					}}
+					inputValue={inputCategory}
+					onInputChange={(event, newInputValue) => {
+						setInputCategory(() => newInputValue)
+					}}
 					disablePortal
 					id='combo-box-demo'
-					options={['hei', 'ui']}
+					options={categories}
 					sx={{ width: 300, flexGrow: '1' }}
 					renderInput={params => <TextField {...params} label='Kategori' />}
 				/>
-				<MyButton variant='contained'>Search!</MyButton>
+				<MyButton variant='contained' disabled={!category && true}>
+					<Link to={`/category?q=${category}`}>Search!</Link>
+				</MyButton>
 			</Wrapper>
 		</MyContainer>
 	)
