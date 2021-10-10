587bc1a66f9dc88f13c17c67889ae50acfda7f68
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-10 14:43:16 +0700

    fix some styles.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,14 @@ const NavBar = () => {
 		<nav className={styles.navBg}>
 			<div className={styles.navbar}>
 				{slug && (
-					<a className={styles.navButton} href='/'>
-						👈🏿
-					</a>
+					// <a className={styles.navButton} href='/'>
+					// 	<img src={process.env.PUBLIC_URL + '/images/left-arrow-Icon lg.png'} alt='' />
+					// </a>
+					<img
+						className={styles.navButton}
+						src={process.env.PUBLIC_URL + '/images/left-arrow-Icon lg.png'}
+						alt=''
+					/>
 				)}
 				<h1 className={styles.logo}>Chuck Norris</h1>
 			</div>
