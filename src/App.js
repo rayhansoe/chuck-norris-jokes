bc2d0acc5e0634639268bacea6e7eafe2352dba8
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-11 04:50:19 +0700

    App.js for routing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-function App() {
+import './global.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SearchResultPage from './pages/SearchResultPage'
+import CategoryResultPage from './pages/CategoryResultPage'
+
+const NotFound = props => <h1>Errror!!</h1>
+
+const App = () => {
 	return (
-		<div className='App'>
-			<h2>Hello World</h2>
-		</div>
+		<Router>
+			<div className='container'>
+				<Switch>
+					<Route path='/' exact component={HomePage} />
+					<Route path='/search' component={SearchResultPage} />
+					<Route path='/category' component={CategoryResultPage} />
+					<Route component={NotFound} />
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
