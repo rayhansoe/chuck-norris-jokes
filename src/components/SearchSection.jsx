a9a4a674ff6cec2e1c351fa7ea1b65954fbaf6b8
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-20 00:13:11 +0700

    add some logic for SearchSection

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -2,13 +2,26 @@ import TextField from '@mui/material/TextField'
 import MyButton from './commons/MyButton'
 import Wrapper from './commons/Wrapper'
 import MyContainer from './commons/MyContainer'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SearchSection = () => {
+	const [keyWord, setKeyWord] = useState(() => '')
 	return (
 		<MyContainer>
 			<Wrapper justifyContent='space-between'>
-				<TextField sx={{ flexGrow: 1 }} variant='outlined' label='Search...' />
-				<MyButton variant='contained'>Search!</MyButton>
+				<TextField
+					sx={{ flexGrow: 1 }}
+					variant='outlined'
+					label='Search...'
+					value={keyWord}
+					onChange={e => {
+						setKeyWord(() => e.target.value)
+					}}
+				/>
+				<MyButton variant='contained' disabled={!keyWord && true}>
+					<Link to={`/search?q=${keyWord}`}>Search!</Link>
+				</MyButton>
 			</Wrapper>
 		</MyContainer>
 	)
