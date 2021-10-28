1d4614df347c6425183dc1b8e5b14880747dc12e
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-28 14:46:36 +0700

    add logic for randomJokes on JokesSection.

--- src/components/JokesSection.jsx
+++ src/components/JokesSection.jsx
@@ -2,15 +2,29 @@ import MyContainer from './commons/MyContainer'
 import Wrapper from './commons/Wrapper'
 import MyText from './commons/MyText'
 import MyButton from './commons/MyButton'
+import { useState, useEffect } from 'react'
+import { randomJokes } from '../tools'
 
 const JokesSection = () => {
-	const jokes = '“Chuck Norris is so awesome when he touches your house it burns down.”'
+	const [jokes, setJokes] = useState(() => '')
+	const [count, setCount] = useState(() => 1)
+
+	useEffect(() => {
+		if (count) {
+			randomJokes().then(r => setJokes(() => r.value))
+		}
+	}, [count])
+
+	const handleClick = () => setCount(c => c + 1)
+
 	return (
 		<MyContainer>
 			<Wrapper type='column' alignItems='center' gap='1.5rem'>
 				<img src={process.env.PUBLIC_URL + '/images/chuck-norris 1.png'} alt='chuck norris' />
 				<MyText>{jokes}</MyText>
-				<MyButton variant='contained'>Another!</MyButton>
+				<MyButton onClick={handleClick} variant='contained'>
+					Another!
+				</MyButton>
 			</Wrapper>
 		</MyContainer>
 	)
