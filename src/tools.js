a74f2a061acacac8a3a414c891808c9b0593f41b
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-28 16:36:58 +0700

    add jokesByQuerySearch function.

--- src/tools.js
+++ src/tools.js
@@ -3,3 +3,9 @@ export const randomJokes = async () => {
 	const response = await data.json()
 	return response
 }
+
+export const jokesByQuerySearch = async q => {
+	const data = await fetch(`https://api.chucknorris.io/jokes/search?query=${q}`)
+	const response = await data.json()
+	return response
+}
