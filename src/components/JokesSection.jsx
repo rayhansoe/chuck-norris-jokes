556e8f1b8f446949ef01b6403a739c6f328c346a
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-29 11:50:16 +0700

    fix ui title.

--- src/components/JokesSection.jsx
+++ src/components/JokesSection.jsx
@@ -2,13 +2,14 @@ import MyContainer from './commons/MyContainer'
 import Wrapper from './commons/Wrapper'
 import MyText from './commons/MyText'
 import MyButton from './commons/MyButton'
+import MyTitle from './commons/MyTitle'
 
 const JokesSection = ({ type, jokes, handleClick, query }) => {
 	return (
 		<MyContainer>
 			<Wrapper type='column' alignItems='center' gap='1.5rem'>
 				<img src={process.env.PUBLIC_URL + '/images/chuck-norris 1.png'} alt='chuck norris' />
-				<h1>{query}</h1>
+				{query && <MyTitle>{query}</MyTitle>}
 				{!type ? (
 					<>
 						<MyText>"{jokes}"</MyText>
@@ -17,7 +18,7 @@ const JokesSection = ({ type, jokes, handleClick, query }) => {
 						</MyButton>
 					</>
 				) : (
-					jokes.map(joke => <MyText key={joke.id}>{joke.value}</MyText>)
+					jokes.map(joke => <MyText key={joke.id}>{`"${joke.value}"`}</MyText>)
 				)}
 			</Wrapper>
 		</MyContainer>
---
+++ src/components/commons/MyTitle.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const MyTitle = styled.p`
+	font-size: 1rem;
+	font-weight: 600;
+	color: #b45309;
+	text-align: center;
+	letter-spacing: normal;
+	text-transform: capitalize;
+`
+
+export default MyTitle
