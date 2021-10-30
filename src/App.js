c4fa99a4cd800dc540a082937cecd7ef6c97daaa
bukanrayhan <rayhan.rhssoe2@gmail.com>
2021-10-30 12:07:43 +0700

    set up notfoundpage.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SearchResultPage from './pages/SearchResultPage'
 import CategoryResultPage from './pages/CategoryResultPage'
-
-const NotFound = props => <h1>Errror!!</h1>
+import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
 	return (
@@ -12,9 +11,9 @@ const App = () => {
 			<div className='container'>
 				<Switch>
 					<Route path='/' exact component={HomePage} />
-					<Route path='/search' component={SearchResultPage} />
-					<Route path='/category' component={CategoryResultPage} />
-					<Route component={NotFound} />
+					<Route path='/search' exact component={SearchResultPage} />
+					<Route path='/category' exact component={CategoryResultPage} />
+					<Route component={NotFoundPage} />
 				</Switch>
 			</div>
 		</Router>
